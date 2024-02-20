a0bf8dd0018722d2606e60d6553786e26eac7f0d
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-20 12:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function makeid(l) {
-  var res="";
-		var rand="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	for(let i=0;i<l;i++){
-		res+=rand.charAt(
-			Math.floor(
-			Math.random()*rand.length;
-		)
-	  )	
-	}
-	return res;
+    var res = "";
+    var rand = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < l; i++) {
+        res += rand.charAt(
+            Math.floor(
+                Math.random() * rand.length
+            )
+        );
+    }
+    return res;
 }
 
 // Do not change the code below.
