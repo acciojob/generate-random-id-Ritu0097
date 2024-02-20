66849d16ff7ca092c021cb10d32121a4fb9bb007
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-20 12:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
-  // write your code here
+  let res="";
+		let rand="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	for(let i=0;i<l;i++){
+		res=res+rand.charAt(Math.floor(
+			Math.random()*rand.length;
+		)
+	  )	
+	}
 }
 
-// Do not change the code below.
+Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
